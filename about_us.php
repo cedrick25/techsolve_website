269f5dc42715b1cc64df338dc7473d989e38c4dd
cedrick25 <dery.cedrick@gmail.com>
2021-10-22 14:18:41 +0800

    techsolve website

---
+++ about_us.php
@@ -0,0 +1,33 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+    <div class="content-wrapper">
+	    <div class="container">
+
+	      <!-- Main content -->
+	      <section class="content">
+	        <div class="row">
+                <div class="col-sm-6">
+                    <h1 id="About us"> About us </h1>
+                    <p>Techsolve Systems Solutions Inc. is a start up company established in 2019 that aims to serve excellence in 
+                        providing end-to-end technology solutions that caters various dilemma of small, medium and enterprise 
+                        companies in the Philippines.<br><br>
+
+                        We are 100% Filipino owned corporation registered in Securities and Exchange Commission that consists of 
+                        Licensed Engineers, IT Specialist, Certified Technical Engineers, Enterprise System Integrators, 
+                        System Analysts, Network Engineers, and Computer Programmers that 
+                        focuses on various programming languages.<br><br>
+
+                        As technology evolves, our technical persons continuously strive to learn and undergo diverse technology 
+                        training courses to cope up with the constant technological development and to serve our clients the best way 
+                        that we can.</p>
+</p>
+                </div>
+            </div>
+</div>
+
+</div>
+
